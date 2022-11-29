fa5d429041386b3d6640390e347fd8972f0a20a4
elhajuojy <souirimehdi311@gmail.com>
2022-11-29 17:57:48 +0100

    change files directory to core folder 🦊 fix some problems 🐬

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-require  "../vendor/autoload.php";
+require "../vendor/autoload.php";
 use HtmlElements\Button;
 use HtmlElements\Form;
 use HtmlElements\Htmlscaffold;
@@ -8,22 +8,9 @@ use HtmlElements\Input;
 use HtmlElements\Header;
 
 
-$form1 = new Form(
-    childern: [
-        (new Input("firstname","text","firstname"))->rander(),
-        (new Input("password","password","password"))->rander(),
-        (new Button("submit","click me ","btn"))->rander(),
-    ],
-    method:"get" ,
-    action:"#",
-
-);
-
-
-
 $arrayStyle = [
-    "assets/css/style.css",
-    "assets/css/style.css"
+    "../assets/css/style.css",
+    "../assets/css/style.css"
 ];
 
 
@@ -33,9 +20,19 @@ $Home = new Htmlscaffold(
     title: "Home Page",
     linkStyle: $arrayStyle,
     body: [
-        (new Header("welcome to our project","h1","center"))->rander(),
-        $form1->rander(),
-        (new Image("assets/images/image.jpg","image"))->rander(),
+        (new Header(title: "welcome to our project",h: "h1",margin: "center"))->rander(),
+        (new Form(
+            childern: [
+                (new Input("firstname","text","firstname"))->rander(),
+                (new Input("password","password","password"))->rander(),
+                (new Button("submit","click me","btn"))->rander(),
+            ],
+            method:"get" ,
+            action:"#",
+
+        ))->rander(),
+
+        (new Image("../assets/images/image.jpg","image"))->rander(),
     ],
 
 );
