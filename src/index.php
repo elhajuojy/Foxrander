3381316420b31b87dd7b6d16867df76dd61a88ed
elhajuojy <souirimehdi311@gmail.com>
2022-11-29 14:57:59 +0100

    re build the way we create out object 😒

--- src/index.php
+++ src/index.php
@@ -6,32 +6,39 @@ use Model\Form;
 use Model\Input;
 use Model\Button;
 use Model\Image;
-
-use Model\Style;
 use Model\Title;
-
-$style = new Style("#ccccff","blue","20px","arial","1px solid black","10px","50%");
-
-
-$form1 = new Form();
-$form1->setAction("#");
-$form1->setMethod("get");
-
-$inputName = new Input("firstname","text","firstname");
-$inputpassword = new Input("password","password","password");
-$btn1 = new Button("submit","click me ","btn");
+use Model\Htmlscaffold;
 
 
-$form1->addElement($inputName);
-$form1->addElement($inputpassword);
-$form1->addElement($btn1);
+$form1 = new Form(
+    childern: [
+        (new Input("firstname","text","firstname"))->rander(),
+        (new Input("password","password","password"))->rander(),
+        (new Button("submit","click me ","btn"))->rander(),
+    ],
+    method:"get" ,
+    action:"#",
 
-$title = new Title("welcome to our project","h1","center");
-echo $title->rander();
-echo $style->rander();
-echo $form1->rander();
-$img = new Image("image.jpg","image");
+);
 
-echo $form1->rander();
-$img = new Image("image.png","image");
-echo $img->rander();
+$arrayStyle = [
+    "../assets/css/style.css",
+    "../assets/css/style.css"
+];
+
+
+
+
+$Home = new Htmlscaffold(
+    title: "Home Page",
+    linkStyle: $arrayStyle,
+    body: [
+        (new Title("welcome to our project","h1","center"))->rander(),
+        $form1->rander(),
+        (new Image("image.png","image"))->rander(),
+    ],
+
+);
+
+echo $Home->rander();
+
