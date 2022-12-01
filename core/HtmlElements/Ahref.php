22372fef4f5fcff16bd4893f96e19ccad3f2a9cd
achchaimae <achchaimae1@gmail.com>
2022-12-01 15:32:43 +0100

    create href element 📌

---
+++ core/HtmlElements/Ahref.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HtmlElements ;
+
+class Ahref extends BaseElement implements  IElmenet
+{
+    private string $link;
+    private string $message;
+
+
+
+    public function __construct( string $link ="",string $message="" , string $className= '',string $class_id='')
+    {
+        $this->link = $link;
+        $this->message=$message;
+        parent::__construct(className: $className, id: $class_id);
+    }
+
+
+    public function rander(): string
+    {
+        return " <a href='https:$this->link' class='$this->className' id='$this->id'>
+                 $this->message
+                </a>";
+    }
+}
