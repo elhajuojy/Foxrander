b4f2a68b64f0e320acafd705c37b59e0691f4806
achchaimae <achchaimae1@gmail.com>
2022-12-01 14:33:52 +0100

    add a little check for p element 🌠

--- views/Home.view.php
+++ views/Home.view.php
@@ -8,6 +8,7 @@ use HtmlElements\Image;
 use HtmlElements\Input;
 use HtmlElements\Header;
 use HtmlElements\Div;
+use HtmlElements\P;
 
 
 $arrayStyle = [
@@ -23,12 +24,13 @@ $Home = new Htmlscaffold(
     body: [
 
         (new Header(title: $h1Title ,h: "h1",className:"text-center text-6xl "))->rander(),
+        (new P(text:"it's just a test",className:"text-center"))->rander(),
         (new Header(title: "Foxrander is a Micro frontend framework PHP based on Object-Oriented 
                             Programming which is help developer rander HTML and CSS faster than ever"
             ,h: "h3",))->rander(),
         (new Div(
             children: [
-            (new Image(src:"./assets/images/foxrander.jpg",alt:"image",className: "bg-blue-500 w-9/12 ",))->rander(),
+            (new Image(src:"./assets/images/foxrander.jpg",alt:"image",className: "bg-blue-500 w-9/12  m-auto",))->rander(),
         ],
             className: "w-full m-auto"
         
