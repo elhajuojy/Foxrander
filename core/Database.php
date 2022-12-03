372bbe04c5778f2d527e0a9ba5fc27bba535e591
elhajuojy <souirimehdi311@gmail.com>
2022-12-03 14:37:48 +0100

    Database Rewriting add more function to it 🫠

--- core/Database.php
+++ core/Database.php
@@ -6,6 +6,7 @@ use PDO;
 class Database
 {
     public $connection;
+    public $statement;
 
     public function __construct($config, $username = 'root', $password = '')
     {
@@ -18,10 +19,25 @@ class Database
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
+        $this->statement = $this->connection->prepare($query);
 
-        $statement->execute($params);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public  function  find(){
+        return $this->statement->fetch();
+    }
+    public  function findOrFail(){
+
+        $result = $this->find();
+
+        if(! $result){
+            //do something
+            Router::abort();
+            
+        }
+        return $result;
     }
 }
