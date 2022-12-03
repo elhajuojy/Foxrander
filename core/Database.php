9defc3a32c1c323c05dedcbb1b3aa4640539d631
elhajuojy <souirimehdi311@gmail.com>
2022-12-03 13:07:21 +0100

    Database configuration 💖🚀

---
+++ core/Database.php
@@ -0,0 +1,27 @@
+<?php
+namespace core;
+
+use PDO;
+
+class Database
+{
+    public $connection;
+
+    public function __construct($config, $username = 'root', $password = '')
+    {
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute($params);
+
+        return $statement;
+    }
+}
