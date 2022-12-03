f1f140f6c32cc22145995d347b5397251a92bc47
elhajuojy <souirimehdi311@gmail.com>
2022-12-03 18:04:39 +0100

    fix Texterea some problems🦊🦊

--- core/HtmlElements/Textarea.php
+++ core/HtmlElements/Textarea.php
@@ -9,19 +9,19 @@ class Textarea extends BaseElement implements IElmenet{
     private int $cols;
     private string $text;
 
-    public function  __construct(string $name='',int $rows='',int $cols='',string $text , string $className= '',string $class_id=''){
-         parent::__construct(className: $className, id: $class_id);
-         $this->name=$name ;
-         $this->rows=$rows ;
-         $this->cols=$cols ;
-         $this->text=$text ;
+    public function  __construct(string $text,int $rows=5,int $cols=30,string $name="" , string $className= '',string $class_id=''){
+      $this->name=$name ;
+      $this->rows=$rows ;
+      $this->cols=$cols ;
+      $this->text=$text ;
+      parent::__construct(className: $className, id: $class_id);
     }
 
 
     public function  rander(): string
     {
         // TODO: Implement rander() method.
-        return "<textarea id='$this->id' name='$this->name' rows='$this->rows' cols='$this->cols'>
+        return "<textarea id='$this->id' class='$this->className' name='$this->name' rows='$this->rows' cols='$this->cols'>
           $this->text
         </textarea>";
     }
