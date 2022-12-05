95d9ff4b79024446b05cf7a584988ae864063b5d
elhajuojy <souirimehdi311@gmail.com>
2022-12-05 09:43:37 +0100

    index .

--- index.php
+++ index.php
@@ -10,12 +10,6 @@ require "vendor/autoload.php";
 $routes = require "routes.php";
 
 
-//database Configuration 
-$config = require ("config.php");
-$db = new Database($config['database']);
-$notes = $db->query("select * from notes")->findOrFail();
-
-
 
 //routring 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
